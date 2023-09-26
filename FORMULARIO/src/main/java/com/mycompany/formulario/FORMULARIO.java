8acefc13410c40a3a30fac5e15d1679740bdc996
paaaaattt <145470405+paaaaattt@users.noreply.github.com>
2023-09-26 08:35:22 -0600

    Add files via upload

---
+++ FORMULARIO/src/main/java/com/mycompany/formulario/FORMULARIO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formulario;
+import javax.swing.*;
+
+/**
+ *
+ * @author Yo
+ */
+public class FORMULARIO extends JFrame {
+    public FORMULARIO(){
+        setLayout(null);
+    }
+    public static void main(String[] args){
+        FORMULARIO formulario1=new FORMULARIO();
+        formulario1.setBounds(10,20,400,300);
+        formulario1.setVisible(true);
+        formulario1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+    }
+
